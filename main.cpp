472e7a2f37399d30cf7198bb49b509ec9e1ce1df
Linda Phanvilay <linda.phanvilay@mavs.uta.edu>
2018-09-06 02:58:35 -0500

    Fixed problem with main.cpp printing running perimeter and problem with program not terminating with negative number

--- main.cpp
+++ main.cpp
@@ -8,12 +8,11 @@ double temp;
 for(int i=1;i<=5;i++){
 	cout << "Enter the length of side " << i << " of the polygon: " << endl; 
 	cin >> temp;
-	if (temp > 0)
-	cout << polygon.add_side(temp)  << endl;
-	else{
+	polygon.add_side(temp);
+	if (temp < 0){
 		cout << "A negative length was given, try again " << endl;
-		break;}
+		exit(0);}
 }
-cout << "The perimeter of the " << polygon.get_sides() <<"-sided polygon is" << polygon.get_perimeter() << endl;
+cout << "The perimeter of the " << polygon.get_sides() <<"-sided polygon is " << polygon.get_perimeter() << endl;
 return 0;
 }
